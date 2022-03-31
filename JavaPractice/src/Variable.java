0527bf7bb9fc53eaa3fe51267ac82b22f16b44a8
Indiras Khatri <42945839+Kindiras@users.noreply.github.com>
2022-03-31 17:16:00 -0400

    Add files via upload

---
+++ JavaPractice/src/Variable.java
@@ -0,0 +1,7 @@
+public class Variable {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        int a = (5*10)+20;
+        System.out.println("The variable is");System.out.println(a);
+    }
+}
