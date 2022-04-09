f6934ddc8899e2cc03d79bdc56114f00a4781f3a
Indiras Khatri <indiras.khatri@rockets.utoledo.edu>
2022-04-09 14:55:27 -0400

    removedFolder

--- JavaPractice/src/Variable.java
+++
@@ -1,7 +0,0 @@
-public class Variable {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-        int a = (5*10)+20;
-        System.out.println("The variable is");System.out.println(a);
-    }
-}
